50582808ef85d900c9e2a98efc35e71b344a6bd7
Fabian Fleck <faabianfleeck@gmail.com>
2024-11-10 15:29:54 -0300

    first commit

---
+++ src/main/java/br/com/auto/electrical/AutoElectricalApplication.java
@@ -0,0 +1,13 @@
+package br.com.auto.electrical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoElectricalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoElectricalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/auto/electrical/AutoElectricalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.auto.electrical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoElectricalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
